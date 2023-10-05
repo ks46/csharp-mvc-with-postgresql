bd01aabc7e34adb201019676855b91f8786cac54
konas <konas@LAPTOP-1GHMR8NV>
2023-10-05 23:52:00 +0300

    added DataContext

---
+++ exercise.api/DataContext/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using exercise.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.api.DataContext
+{
+    public class DataContext : DbContext
+    {
+        private string connectionString;
+
+        public DataContext()
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- exercise.api/Program.cs
+++ exercise.api/Program.cs
@@ -1,10 +1,16 @@
+using exercise.api.DataContext;
 using exercise.api.EndPoints;
+using exercise.api.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddDbContext<DataContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
